4491a78fa1d81b36c0dd880370a6b3444b548deb
bilalthepunjabi <bilalthepunjabi@gmail.com>
2021-08-03 20:29:24 +0500

    redefined vuex-install method

--- src/stores/index.js
+++ src/stores/index.js
@@ -9,19 +9,16 @@ const installer = registery;
 export default {
   install: (app, options) => {
     app.use(VueAxios, axios);
-    if (installer.length > 1) {
+    if (installer.length > 0) {
       app.config.globalProperties.$store = {};
-      installer.forEach((store_instance)=>{
-           if(app.config.globalProperties.$store !== undefined){
-             app.config.globalProperties.$store[Object.keys(store_instance)[0]] = store_instance[Object.keys(store_instance)[0]]
-           }
-      })
-    }else{
-      if(installer.length === 1){
-        installer.forEach((store_instance)=>{
+      installer.forEach((store_instance,index)=>{
+        if(index === 0){
           app.use(store_instance[Object.keys(store_instance)[0]]);
-        })
-      }
+        }
+        if(app.config.globalProperties.$store !== undefined){
+          app.config.globalProperties.$store[Object.keys(store_instance)[0]] = store_instance[Object.keys(store_instance)[0]]
+        }
+      })
     }
   }
 };
